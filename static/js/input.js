4220dffd97316ea62a45dba6fdec773c38d0be1e
alvarohcugr <alvarohc@correo.ugr.es>
2024-04-20 19:46:38 +0200

    Drop zone sin errores

--- static/js/input.js
+++ static/js/input.js
@@ -10,27 +10,32 @@ function dropHandler(event) {
   if (event.dataTransfer.items) {
       // Si hay más de un archivo, toma el primero
       if (event.dataTransfer.items.length > 0 && event.dataTransfer.items[0].kind === 'file') {
-          console.log("Hola")
           file = event.dataTransfer.items[0].getAsFile();
       }
   } else {
       // Si no hay items, toma el primer archivo del array de archivos
       if (event.dataTransfer.files.length > 0) {
-        console.log("Hola2")
           file = event.dataTransfer.files[0];
       }
   }
 
   if (file) {
-      var input = document.getElementById('audio_file');
-      var fileNameSpan = document.getElementById('file-name');
+    // Verificar si el archivo es de tipo .wav
+    if (file.type === 'audio/x-wav' || file.type === 'audio/wav') {
+        var input = document.getElementById('audio_file');
+        var fileNameSpan = document.getElementById('file-name');
 
-      var dataTransfer = new DataTransfer();
-      dataTransfer.items.add(file);
-      input.files = dataTransfer.files; // Asigna los archivos al input file
+        var dataTransfer = new DataTransfer();
+        dataTransfer.items.add(file);
+        input.files = dataTransfer.files;
 
-      fileNameSpan.textContent = file.name; // Actualiza el texto del span con el nombre del archivo
+        fileNameSpan.textContent = file.name;
+    } else {
+      console.log(file.type);
+        alert('Por favor, sube un archivo de tipo .wav.');
+    }
   }
+  event.currentTarget.classList.remove('highlight');
 }
 
 function handleDragOver(event) {
@@ -39,12 +44,16 @@ function handleDragOver(event) {
 
 function handleDragEnter(event) {
   event.preventDefault();
-  event.target.classList.add('highlight');
+  event.currentTarget.classList.add('highlight');
 }
 
 function handleDragLeave(event) {
   event.preventDefault();
-  event.target.classList.remove('highlight');
+  /* event.currentTarget.classList.remove('highlight'); */
+  // Verificar si el puntero del ratón ha salido del drop_zone o si ha entrado en otro elemento interno
+  if (!event.relatedTarget || (event.relatedTarget !== this && !this.contains(event.relatedTarget))) {
+    this.classList.remove('highlight');
+}
 }
 
 
